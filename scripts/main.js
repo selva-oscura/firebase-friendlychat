62619cd60a9d172c8b1ae65c89e72256e97bcab0
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-10 21:19:27 -0700

    added code to saveImageMessage (create placeholder message with temporary loading image, upload file to Firebase Storage, and update the chat message with the newly uploaded file's Firebase Storage URI) and setImageUrl (query Firebase Storage for the URL if the image reference begins with gs://)

--- scripts/main.js
+++ scripts/main.js
@@ -71,7 +71,7 @@ FriendlyChat.prototype.loadMessages = function() {
   // Load the last 12 messages and listen for new ones
   var setMessage = function(data){
     var val = data.val();
-    this.displayMessage(data.key, val.name, val.text, val.photoURL, val.imageURL)
+    this.displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl)
   }.bind(this);
   this.messagesRef.limitToLast(12).on('child_added', setMessage);
   this.messagesRef.limitToLast(12).on('child_changed', setMessage);
@@ -102,9 +102,15 @@ FriendlyChat.prototype.saveMessage = function(e) {
 
 // Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
 FriendlyChat.prototype.setImageUrl = function(imageUri, imgElement) {
-  imgElement.src = imageUri;
-
-  // TODO(DEVELOPER): If image is on Firebase Storage, fetch image URL and set img element's src.
+  // If image is on Firebase Storage, fetch image URL and set img element's src.
+  if(imageUri.startsWith('gs://')){
+    imgElement.src = FriendlyChat.LOADING_IMAGE_URL; // Display a loading image first
+    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata){
+      imgElement.src = metadata.downloadURLs[0];
+    });
+  }else{
+    imgElement.src = imageUri;
+  }
 };
 
 // Saves a new message containing an image URI in Firebase.
@@ -126,9 +132,24 @@ FriendlyChat.prototype.saveImageMessage = function(event) {
   }
   // Check if the user is signed-in
   if (this.checkSignedInWithMessage()) {
-
-    // TODO(DEVELOPER): Upload image to Firebase storage and add message.
-
+    // Upload image to Firebase storage and add message.
+    var currentUser = this.auth.currentUser;
+    this.messagesRef.push({
+      name: currentUser.displayName,
+      imageUrl: FriendlyChat.LOADING_IMAGE_URL,
+      photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+    }).then(function(data){
+      // Upload the image to Firebase Storage
+      this.storage.ref(currentUser.uid + '/' + Date.now() + '/' + file.name)
+        .put(file, {contentType: file.type})
+        .then(function(snapshot){
+          // Get the file's Storage URI and update the chat message placeholder
+          var filePath = snapshot.metadata.fullPath;
+          data.update({imageUrl: this.storage.ref(filePath).toString()});
+        }.bind(this)).catch(function(error){
+          console.error("There was an error uploading a file to Firebase Storage:", error)
+        });
+    }.bind(this));
   }
 };
 
